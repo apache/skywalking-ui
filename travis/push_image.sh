20f0bc4bc8c8067f920fe992fdf5ef12cc3a7397
zhangxin <zhangxin@oneapm.com>
2017-04-25 19:33:07 +0800

    push docker image when skywalking-ui is tagged

--- travis/push_image.sh
+++ travis/push_image.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
 check_pull_is_tagged() {
-  if [ "${TRAVIS_TAG}" == "" ]; then
+  if [ "${TRAVIS_TAG}" = "" ]; then
     return 1
   else
+    echo "This build was started by the tag ${TRAVIS_TAG}, push image"
     return 0
   fi
 }
 
 check_branch_is_master(){
-    if [ "${TRAVIS_BRANCH}" == "master" ]; then
+    if [ "${TRAVIS_BRANCH}" = "master" ]; then
+        echo "Travis branch is master"
         return 0;
     else
+        echo "Travis branch is not master"
         return 1;
     fi
 }
